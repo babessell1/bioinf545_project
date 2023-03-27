109f96c08bcf0d65ca065468b7b438795dba815d
babessell1 <babessell@gmail.com>
2023-03-26 22:13:15 -0400

    [new] trim, build genomes

--- bash/download.sh
+++ bash/download.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=5:00:00
 #SBATCH --account=bioinf545w23_class
 #SBATCH --partition=standard
-#SBATCH --output=../logs/download.log
+#SBATCH --output=logs/download.log
 
 module load Bioinformatics
 module load sratoolkit/2.10.9-udmejx7
@@ -17,17 +17,37 @@ mkdir -p ../data/genomes
 mkdir -p ../data/untrimmed_reads
 
 #get genomes
+# sheep
 curl -OJX \
---output-dir data/genomes/ \
-GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000298735.2/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000298735.2.zip" -H "Accept: application/zip"
+GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000298735.2/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,SEQUENCE_REPORT&filename=GCF_000298735.2.zip" -H "Accept: application/zip"
 
+unzip ./*.zip
+mv ./ncbi_dataset/data/GCF_000298735.2/*.fna ../data/genomes/sheep.fna
+mv ./ncbi_dataset/data/GCF_000298735.2/*.gff ../data/genomes/sheep.gff
+rm -r ./ncbi_dataset
+rm ./*.zip
+rm ./*.md
+
+gzip ./*.fna
+gzip ./*.gff
+
+# cow
 curl -OJX \
---output-dir data/genomes/ \
 GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000003055.6/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,SEQUENCE_REPORT&filename=GCF_000003055.6.zip" -H "Accept: application/zip"
+
+unzip ./*.zip
+mv ./ncbi_dataset/data/GCF_000003055.6/*.fna ../data/genomes/cow.fna
+mv ./ncbi_dataset/data/GCF_000003055.6/*.gff ../data/genomes/cow.gff
+rm -r ./ncbi_dataset
+rm ./*.zip
+rm ./*.md
+
+gzip ./*.fna
+gzip ./*.gff
 
 # get rnaseq
 while read srr; do
-    fasterq-dump \
+    fastq-dump \
     --gzip \
     --readids \
     --read-filter pass \
@@ -36,5 +56,5 @@ while read srr; do
     --clip \
     --outdir ../data/untrimmed_reads \
     $srr
-done <srr.txt
+done <../data/srr.txt
 
