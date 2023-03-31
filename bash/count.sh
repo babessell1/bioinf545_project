7af3aa5ab134446f8f9db06f54fb71c178bc24a3
bbessell <babessell@gmail.com>
2023-03-30 21:46:57 -0400

    [new] R scripts for DGE analysis

--- bash/count.sh
+++ bash/count.sh
@@ -32,6 +32,7 @@ htseq-count \
     -f=bam \
     -s=no \
     -r=pos \
+    -i=gene_name \
     "../data/star_${2}/${2}_${1}_Aligned.sortedByCoord.out.bam" \
     "../data/genomes/${2}_qortsfix.gtf" \
     > "../data/${2}_gene_cnt/${2}_${1}_gene_cnt.htseq.out"
@@ -56,6 +57,7 @@ htseq-count \
     -f=bam \
     -s=no \
     -r=pos \
+    -i=gene_name \
     "../data/${2}_aligned_rmdups/${2}_${1}_aligned_rmdup.bam" \
     "../data/genomes/${2}_qortsfix.gtf" \
     > "../data/${2}_gene_cnt/${2}_${1}_gene_cnt_rmdup.htseq.out"
